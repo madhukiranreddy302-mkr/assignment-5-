572dc5d003b8a4136818996d40f37a0d610b4059
Madhu Kiran Reddy <madhukiranreddy302@gmail.com>
2025-09-16 13:47:39 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+first_five = numbers[:5]
+reversed_list = first_five[::-1]
+print("Original list:",numbers)
+print("Extracted first five elements:", first_five)
+print("Reversed first five elements:", reversed_list)
